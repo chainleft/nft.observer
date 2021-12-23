e24218745136f57a06f83fa656e451094928d7e2
chainleft <89662508+chainleft@users.noreply.github.com>
2021-12-23 04:18:10 +0100

    Update nft_history.py

--- nft_history.py
+++ nft_history.py
@@ -9,30 +9,33 @@ import csv
 #from load_css import local_css
 #local_css("style.css")
 
+def pull_organize():
+  df = pd.read_csv('nfts_edit.csv')
+  df = df.replace(np.nan, '', regex=True)
+  df = df[df["Network"] == "Ethereum"]
+  df['address'] = df['Contract'].str.split("https://etherscan.io/address/").str[1]
+  df['opensea_coll_slug'] = df['OpenSea'].str.split("https://opensea.io/collection/").str[1]
+  return df
+
+def build_choices(df,column):
+  choices = list(set(df[column]))
+  choices.append('')
+  choices.sort()
+  return choices
 
 st.title('NFT Collection History')
 
-df = pd.read_csv('nfts.csv')
-df = df.replace(np.nan, '', regex=True)
-
-df['address'] = df['Contract'].str.split("https://etherscan.io/address/").str[1]
+df = pull_organize()
 
 onoff_options = ["","Off-chain","On-chain"]
-gen_options = ["","Manual","Generative"]
-
-cat_options = list(set(df['Category']))
-cat_options.append('')
-cat_options.sort()
-
-format_options = list(set(df['Format']))
-format_options.append('')
-format_options.sort()
-
-type_options = list(set(df['Type']))
-type_options.append('')
-type_options.sort()
-
+gen_options = ["","Curated","Generative"]
+sft_options = ["Included","Excluded"]
 interact_options = ["","Interactive","Non-interactive"]
+
+cat_options = build_choices(df,'Category')
+format_options = build_choices(df,'Format')
+type_options = build_choices(df,'Type')
+
 
 #detail_options = list(set(df['Detail']))
 #detail_options.append("")
@@ -47,25 +50,34 @@ class ParameterError(Exception):
     pass
 
 try:
-  #Generative or manual production
+  #Non-fungible or semi-fungible
+  sft = st.sidebar.checkbox('Include SFTs (editions)?', value=True)
+  #Filter based on sidebar selection
+  if sft == False:
+    df2 = df2[df2["nonfungibleness"] == "Fully nonfungible"]
+    cat_options = build_choices(df2,'Category')
+    format_options = build_choices(df2,'Format')
+    type_options = build_choices(df2,'Type')
+
+  #Tokenness
+  nontokens = st.sidebar.checkbox('Include non-tokens?', value=False)
+  #Filter based on sidebar selection
+  if nontokens == False:
+    df2 = df2[df2["transferrable"] == True]
+
+  #Generative or curated production
   gen = st.sidebar.selectbox(
-      'Generative or manually produced?',
+      'Generative or curated?',
        gen_options)
   #Filter based on sidebar selection
-  if gen == "Manual":
-    df2 = df2[df2["Generative?"] == False]
+  if gen == "Curated":
+    df2 = df2[df2["Generative"] == False]
     onoff_options = ["","Off-chain"]
-    cat_options = list(set(df2['Category']))
-    cat_options.append('')
-    cat_options.sort()
-    format_options = list(set(df2['Format']))
-    format_options.append('')
-    format_options.sort()
-    type_options = list(set(df2['Type']))
-    type_options.append('')
-    type_options.sort()
+    cat_options = build_choices(df2,'Category')
+    format_options = build_choices(df2,'Format')
+    type_options = build_choices(df2,'Type')
   elif gen == "Generative":
-    df2 = df2[df2["Generative?"] == True]
+    df2 = df2[df2["Generative"] == True]
 
   #Generated on or off chain
   onoff = st.sidebar.selectbox(
@@ -73,28 +85,16 @@ try:
        onoff_options)
   #Filter based on sidebar selection
   if onoff == "On-chain":
-    df2 = df2[df2["Generated on chain?"] == True]
-    cat_options = list(set(df2['Category']))
-    cat_options.append('')
-    cat_options.sort()
-    format_options = list(set(df2['Format']))
-    format_options.append('')
-    format_options.sort()
-    type_options = list(set(df2['Type']))
-    type_options.append('')
-    type_options.sort()
+    df2 = df2[df2["OnChain"] == True]
+    cat_options = build_choices(df2,'Category')
+    format_options = build_choices(df2,'Format')
+    type_options = build_choices(df2,'Type')
 
   elif onoff == "Off-chain":
-    df2 = df2[df2["Generated on chain?"] == False]
-    cat_options = list(set(df2['Category']))
-    cat_options.append('')
-    cat_options.sort()
-    format_options = list(set(df2['Format']))
-    format_options.append('')
-    format_options.sort()
-    type_options = list(set(df2['Type']))
-    type_options.append('')
-    type_options.sort()
+    df2 = df2[df2["OnChain"] == False]
+    cat_options = build_choices(df2,'Category')
+    format_options = build_choices(df2,'Format')
+    type_options = build_choices(df2,'Type')
 
   #Category
   cat = st.sidebar.selectbox(
@@ -103,12 +103,8 @@ try:
   #Filter based on sidebar selection
   if cat != "":
     df2 = df2[df2["Category"] == cat]
-    format_options = list(set(df2['Format']))
-    format_options.append('')
-    format_options.sort()
-    type_options = list(set(df2['Type']))
-    type_options.append('')
-    type_options.sort()
+    format_options = build_choices(df2,'Format')
+    type_options = build_choices(df2,'Type')
 
   #Format
   fileformat = st.sidebar.selectbox(
@@ -117,9 +113,7 @@ try:
   #Filter based on sidebar selection
   if fileformat != "":
     df2 = df2[df2["Format"] == fileformat]
-    type_options = list(set(df2['Type']))
-    type_options.append('')
-    type_options.sort()
+    type_options = build_choices(df2,'Type')
 
   #Subtype
   type_selected = st.sidebar.selectbox(
@@ -148,9 +142,15 @@ try:
 
   df2 = df2.sort_values(by=['timestamp'])
 
+  text_sft = ""
+  if sft == False: text_sft = " (excluding editioned SFTs)"
+
+  text_nontokens = ""
+  if nontokens == True: text_nontokens = " (including non-transferrable entities)"
+
   if onoff=="On-chain": text_onoff_gen = "on-chain generated"
   elif onoff=="Off-chain": text_onoff_gen = "off-chain generated"
-  elif gen=="Manual": text_onoff_gen = "manually created"
+  elif gen=="Curated": text_onoff_gen = "curated"
   else: text_onoff_gen = onoff.lower() + " " + gen.lower()
 
   text_interactive = interact.lower()
@@ -172,7 +172,7 @@ try:
   text_alltypes = text_format + ' ' + text_cat + ' NFT collection ' + text_type
   if text_type == "based on meta":
     text_alltypes = text_format + ' meta NFT collection'
-  st.write('On Ethereum network, the first ', text_onoff_gen, text_interactive, text_alltypes, 'was:')
+  st.write('On Ethereum network, the first ', text_onoff_gen, text_interactive, text_alltypes, text_sft, text_nontokens, 'was:')
   st.subheader(df2['Title'].iloc[0])
   st.text("")
   st.write(df2['Title'].iloc[0],'was created on',df2['Date'].iloc[0],'.')
@@ -197,8 +197,10 @@ try:
   st.write(text_opensea)
   st.write(text_contract)
   st.write(text_twitter)
-  #st.image("https://ipfs.io/ipfs/QmZH7MZK6XGELZNhHre2LJiBcXFpsi1wzhmx1MGcApahnD",width=400, # Manually Adjust the width of the image as per requirement)
-
+  # Manually adjust the width of the image as per requirement
+  if df2['image'].iloc[0]!='':
+    st.image(df2['image'].iloc[0],width=200, )
+  
   left_column, right_column = st.columns(2)
 
 #  if st.checkbox('Show the full list'):
